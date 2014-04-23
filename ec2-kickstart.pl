065af79f83516d89e845968a2b9b8f94993ec828
Ross Dakin <rossdakin@gmail.com>
2014-04-23 03:36:34 -0400

    Update to use awscli

--- ec2-kickstart.pl
+++ ec2-kickstart.pl
@@ -6,9 +6,11 @@ use File::Basename;
 
 my $instance = "i-XXXXXXXX";
 my $host_name = "my.public.host.name";
+my $hosted_zone = "/hostedzone/XXXXXXXXXXXXXX";
 my $dns_ttl = 60; # seconds
 
 my $vol_name = "MyVolume";
+my $vol_id = "vol-XXXXXXXX";
 my $snap_name = "$vol_name (backup)";
 my $snap_desc = "Pre-attachment backup (" . basename($0) . ")";
 
@@ -21,60 +23,69 @@ print "---\n";
 
 # start the sleeping instance
 print "Starting instance $instance...\n";
-`ec2start $instance` or die "Couldn't start instance $instance";
+$out = `aws ec2 start-instances --instance-ids $instance` or die "Couldn't start instance $instance: $out";
 
 
-# get instance IP
-print "Getting public IP address of new instance ($instance)...\n";
+# get instance DNS name
+print "Getting public IP address of instance ($instance)...\n";
+my $ip = undef;
+while(!$ip) {
+    sleep 1;
+    $out = `aws ec2 describe-instances --instance-ids $instance`;
+    $out =~ m/"PublicIpAddress": "(.+)"/;
+    $ip = $1;
+}
 
-$out = `ec2-describe-instances`;
-$out =~ m/INSTANCE\s+$instance(?:\s+\S+){11}\s+(\d+\.\d+\.\d+\.\d+)/;
-
-my $ip = $1 or die "Could not get new instance IP";
-
-
-# update DNS (Route53) using:
-# https://github.com/tkuhlman/cirrus/blob/master/bin/update_host.py
+# update DNS (Route53)
+print "Updating DNS (pointing $host_name to $ip)...\n";
+my $change_batch = "'\
+          {\
+            \"Comment\": \"Updating $host_name to $ip\",\
+            \"Changes\": [\
+              {\
+                \"Action\": \"UPSERT\",\
+                \"ResourceRecordSet\": {\
+                  \"Name\": \"$host_name\",\
+                  \"Type\": \"A\",\
+                  \"TTL\": $dns_ttl,\
+                  \"ResourceRecords\": [\
+                    {\
+                      \"Value\": \"$ip\"\
+                    }\
+                  ]\
+                }\
+              }\
+            ]\
+          }\
+'";
 $cmd = join(' ',
-            "update_host.py",
-            "$host_name",
-            "$host_name",
-            "-t $dns_ttl",
-            "-a $ip");
+            "aws route53 change-resource-record-sets",
+            "--hosted-zone-id $hosted_zone",
+            "--change-batch $change_batch");
 
-print `$cmd`;
+`$cmd`;
 
 
-# get volume ID from name
-print "Fetching description of EBS volume ($vol_name)...\n";
-
-$out = `ec2-describe-volumes -F tag:Name=$vol_name`;
-$out =~ m/VOLUME\t(vol-\w+)\t/;
-
-my $vol = $1 or die "Could not determine EBS volume ID from name ($vol_name)";
-
-
-# create snapshot of volume before attaching it
-print "Creating snapshot of volume ($vol)...\n";
+# create snapshot of volume
+print "Creating snapshot of volume ($vol_id)...\n";
 $cmd = join(' ',
-            "ec2-create-snapshot",
-            "$vol",
-            "-d '$snap_desc'");
+            "aws ec2 create-snapshot",
+            "--volume-id $vol_id",
+            "--description '$snap_desc'");
 
-$out = `$cmd` or die "Failed to create snapshot of volume $vol";
-$out =~ m/SNAPSHOT\t(snap-\w+)\t/;
-
-my $snap = $1 or die "Could not determine snapshot ID";
+$out = `$cmd` or die "Failed to create snapshot of volume $vol_id";
+$out =~ m/"SnapshotId": "(.+)"/;
+my $snap = $1 or die "Could not determine snapshot ID: $out";
 
 
 # give new snapshot a name
 print "Giving new snapshot ($snap) a name: '$snap_name'...\n";
 $cmd = join(' ',
-            "ec2-create-tags",
-            "$snap",
-            "--tag Name='$snap_name'");
+            "aws ec2 create-tags",
+            "--resources $snap",
+            "--tags Key=Name,Value='$snap_name'");
 
-$out = `$cmd` or die "Failed to give snapshot $snap the name '$snap_name'";
+$out = `$cmd` or die "Failed to give snapshot $snap the name '$snap_name': $out";
 
 
 # success
