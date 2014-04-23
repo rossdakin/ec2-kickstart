515a10a08f70b2f0f224a7d6f2434e5076ba694d
Ross Dakin <rossdakin@gmail.com>
2014-04-23 06:35:51 +0000

    New technique: just start a sleeping instance
    Since AWS doesn't charge while instances are off, just stop and start one instance (keeps things going faster).
    
    Also, update DNS so we don't have to pay for an elastic EC2 IP.

--- ec2-kickstart.pl
+++ ec2-kickstart.pl
@@ -4,13 +4,10 @@ use strict;
 use warnings;
 use File::Basename;
 
-my $ami = "ami-XXXXXXXX";
-my $group = "MY-GROUP-NAME";
-my $keypair = "MY-KEY-NAME";
-my $zone = "us-east-1b";
-my $type = "m1.small";
+my $instance = "i-XXXXXXXX";
 my $host_name = "my.public.host.name";
 my $dns_ttl = 60; # seconds
+
 my $vol_name = "MyVolume";
 my $snap_name = "$vol_name (backup)";
 my $snap_desc = "Pre-attachment backup (" . basename($0) . ")";
@@ -21,23 +18,10 @@ my ($cmd, $out);
 
 print "---\n";
 
-# run instance
-print "Starting new instance from AMI ($ami)...\n";
 
-$cmd = join(' ',
-            "ec2-run-instances",
-            "$ami",
-            "-g $group",
-            "-n 1",
-            "-k $keypair",
-            "-t $type",
-            "-z $zone",
-            "--instance-initiated-shutdown-behavior terminate");
-
-$out = `$cmd` or die "Failed to start an instance of $ami";
-
-$out =~ m/INSTANCE\t(i-\w+)/;
-my $instance = $1 or die "Could not get new instance ID";
+# start the sleeping instance
+print "Starting instance $instance...\n";
+`ec2start $instance` or die "Couldn't start instance $instance";
 
 
 # get instance IP
@@ -49,7 +33,8 @@ $out =~ m/INSTANCE\s+$instance(?:\s+\S+){11}\s+(\d+\.\d+\.\d+\.\d+)/;
 my $ip = $1 or die "Could not get new instance IP";
 
 
-# update DNS
+# update DNS (Route53) using:
+# https://github.com/tkuhlman/cirrus/blob/master/bin/update_host.py
 $cmd = join(' ',
             "update_host.py",
             "$host_name",
@@ -60,11 +45,6 @@ $cmd = join(' ',
 print `$cmd`;
 
 
-# wait for instance to be "running"
-print "Waiting 30 seconds for new instance ($instance) to be running...\n";
-sleep(30);
-
-
 # get volume ID from name
 print "Fetching description of EBS volume ($vol_name)...\n";
 
@@ -73,32 +53,6 @@ $out =~ m/VOLUME\t(vol-\w+)\t/;
 
 my $vol = $1 or die "Could not determine EBS volume ID from name ($vol_name)";
 
-
-# detach from existing instance
-if ($out =~ m/ATTACHMENT(?:\t[^\t]*){3}\t(\w+)\t/) {
-    my $state = $1;
-
-    print "  Already attached to another instance...\n";
-
-    # detach if attaching/attached
-    `ec2-detach-volume $vol` if ($1 =~ m/attach/);
-
-    # wait for attachment to go away
-    my $ready = 0;
-    do {
-        sleep(3);
-
-        print "  State: $state\n";
-
-        $out = `ec2-describe-volumes -F tag:Name=$vol_name`;
-
-        if ($out =~ m/ATTACHMENT(?:\t[^\t]*){3}\t(\w+)\t/) {
-            $state = $1;
-        } else {
-            $ready = 1;
-        }
-    } while (!$ready);
-}
 
 # create snapshot of volume before attaching it
 print "Creating snapshot of volume ($vol)...\n";
@@ -121,17 +75,6 @@ $cmd = join(' ',
             "--tag Name='$snap_name'");
 
 $out = `$cmd` or die "Failed to give snapshot $snap the name '$snap_name'";
-
-
-# attach
-print "Attaching volume ($vol) to instance ($instance)...\n";
-$cmd = join(' ',
-            "ec2-attach-volume",
-            "$vol",
-            "-i $instance",
-            "-d xvdf");
-
-$out = `$cmd` or die "Failed to attach volume $vol";
 
 
 # success
