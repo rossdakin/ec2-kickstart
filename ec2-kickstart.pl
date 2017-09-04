6c14ce937e2e1b4f35af33274ed76be7dd136b93
Ross Dakin <rossdakin@gmail.com>
2017-09-04 17:22:51 -0400

    Remove DNS sections, start instance last
    My Windows instance is now self-updating its DNS after it loads via a PowerShell script. This is a little slower (DNS isn't updated until after boot) but centralizes the consern of DNS updating so that I can start the instance from places other than this script and ensure that the DNS is updated -- could actually do the same with the volume snapshotting.
    
    This also now snapshots the volume before starting the instance -- unsure why I didn't always do it that way.

--- ec2-kickstart.pl
+++ ec2-kickstart.pl
@@ -5,10 +5,6 @@ use warnings;
 use File::Basename;
 
 my $instance = "i-XXXXXXXX";
-my $host_name = "my.public.host.name";
-my $hosted_zone = "/hostedzone/XXXXXXXXXXXXXX";
-my $dns_ttl = 60; # seconds
-
 my $vol_name = "MyVolume";
 my $vol_id = "vol-XXXXXXXX";
 my $snap_name = "$vol_name (backup)";
@@ -18,52 +14,8 @@ my $snap_desc = "Pre-attachment backup (" . basename($0) . ")";
 
 my ($cmd, $out);
 
-print "---\n";
-
-
-# start the sleeping instance
-print "Starting instance $instance...\n";
-$out = `aws ec2 start-instances --instance-ids $instance` or die "Couldn't start instance $instance: $out";
-
-
-# get instance DNS name
-print "Getting public IP address of instance ($instance)...\n";
-my $ip = undef;
-while(!$ip) {
-    sleep 1;
-    $out = `aws ec2 describe-instances --instance-ids $instance`;
-    $out =~ m/"PublicIpAddress": "(.+)"/;
-    $ip = $1;
-}
-
-# update DNS (Route53)
-print "Updating DNS (pointing $host_name to $ip)...\n";
-my $change_batch = "'\
-          {\
-            \"Comment\": \"Updating $host_name to $ip\",\
-            \"Changes\": [\
-              {\
-                \"Action\": \"UPSERT\",\
-                \"ResourceRecordSet\": {\
-                  \"Name\": \"$host_name\",\
-                  \"Type\": \"A\",\
-                  \"TTL\": $dns_ttl,\
-                  \"ResourceRecords\": [\
-                    {\
-                      \"Value\": \"$ip\"\
-                    }\
-                  ]\
-                }\
-              }\
-            ]\
-          }\
-'";
-$cmd = join(' ',
-            "aws route53 change-resource-record-sets",
-            "--hosted-zone-id $hosted_zone",
-            "--change-batch $change_batch");
-
-`$cmd`;
+my $now = `date`;
+print "---\n$now";
 
 
 # create snapshot of volume
@@ -85,7 +37,13 @@ $cmd = join(' ',
             "--resources $snap",
             "--tags Key=Name,Value='$snap_name'");
 
-$out = `$cmd` or die "Failed to give snapshot $snap the name '$snap_name': $out";
+$out = `$cmd`;
+die "Failed to give snapshot $snap the name '$snap_name': $out" if $out;
+
+
+# start the sleeping instance
+print "Starting instance $instance...\n";
+$out = `aws ec2 start-instances --instance-ids $instance` or die "Couldn't start instance $instance: $out";
 
 
 # success
